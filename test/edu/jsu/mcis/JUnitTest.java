9e092b515d51773a2b48b918710332fece47a3cb
Matthew Minor <mminor1@stu.jsu.edu>
2018-02-21 19:28:23 -0600

    Passing all tests

---
+++ test/edu/jsu/mcis/JUnitTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.jsu.mcis;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author matts
+ */
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    /*
+    @Test public void testClassMethod1() {
+        assertEquals(TestClass.testMethod1(), "Hello, World!"); 
+    } 
+ 
+    @Test public void testClassMethod2() {
+        assertEquals(TestClass.testMethod2(), "Hello, World!"); 
+    }
+*/
+}
